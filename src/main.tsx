8a55f5466929df53a018777cfaf99de8145be850
vincepg13 <se20basketballcamp@yahoo.com>
2025-04-27 11:22:48 +0100

    Fixed Axios Configuration

--- src/main.tsx
+++ src/main.tsx
@@ -13,28 +13,16 @@ function Container() {
   useEffect(() => {
     if (token === "") {
       if (import.meta.env.MODE === "development") {
-        const username = import.meta.env.VITE_REACT_APP_USER;
-        const password = import.meta.env.VITE_REACT_APP_PASSWORD;
         axios.defaults.auth = {
-          username,
-          password,
+          username: import.meta.env.VITE_REACT_APP_USER,
+          password: import.meta.env.VITE_REACT_APP_PASSWORD,
         };
-        console.log(
-          "DEV MODE - set default username and password ",
-          axios.defaults.auth
-        );
-        setAxiosInstance(axios); // ✅ Add this here too
+        setAxiosInstance(axios);
         setToken("dev mode");
       } else {
         axios.get(import.meta.env.VITE_TOKEN_PATH).then((r) => {
           axios.defaults.headers["X-userToken"] = r.data.result.sessionToken;
-          console.log(
-            "PROD MODE - received ServiceNow token: ",
-            r.data.result.sessionToken
-          );
-
-          const token =
-            r.data.result.username == "guest" ? "" : r.data.result.token;
+          const token = r.data.result.username == "guest" ? "" : r.data.result.token;
           setAxiosInstance(axios);
           setToken(token);
         });
